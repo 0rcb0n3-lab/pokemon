3ed993806fb9ab88467dff3d169c3d1f4a04d0ad
0rcb0n3-lab <0rcb0n3@gmail.com>
2026-08-03 22:34:47 +0300

    add PokemonEntity model

--- pokemon_entities/admin.py
+++ pokemon_entities/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Pokemon
+from .models import Pokemon, PokemonEntity
 
 
 admin.site.register(Pokemon)
+admin.site.register(PokemonEntity)
+
