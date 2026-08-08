92fd0eebc06b883e13776fda09937d729b4db293
maxd31m0s <0rcb0n3@gmail.com>
2026-08-08 18:36:45 +0300

    added previous evolution

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -8,6 +8,14 @@ class Pokemon(models.Model):
     title_en = models.CharField(max_length=200, blank=True)
     title_jp = models.CharField(max_length=200, blank=True)
     description = models.TextField(blank=True)
+    previous_evolution = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='next_evolutions',
+        verbose_name='Из кого эволюционировал',
+    )
 
     def __str__(self):
         return f"{self.title}"
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -88,13 +88,22 @@ def show_pokemon(request, pokemon_id):
 
     img_url = request.build_absolute_uri(pokemon.image.url) if pokemon.image else None
 
+    previous_evolution_info = None
+    if pokemon.previous_evolution:
+        previous_evolution_info = {
+            'pokemon_id': pokemon.previous_evolution.id,
+            'img_url': request.build_absolute_uri(pokemon.previous_evolution.image.url) if pokemon.previous_evolution.image else None,
+            'title_ru': pokemon.previous_evolution.title,
+        }
+
     pokemon_on_page = {
         'pokemon_id': pokemon.id,
         'img_url': img_url,
         'title_ru': pokemon.title,
-        'title_en' : pokemon.title_en,
+        'title_en': pokemon.title_en,
         'title_jp': pokemon.title_jp,
-        'description' :pokemon.description,
+        'description': pokemon.description,
+        'previous_evolution': previous_evolution_info
     }
 
     return render(request, 'pokemon.html', context={
